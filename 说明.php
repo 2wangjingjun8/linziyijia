5a82d8f699480991e77ead6190d2466ee139807f
2wangjingjun8 <39975830+2wangjingjun8@users.noreply.github.com>
2020-01-04 14:18:39 +0800

    Update 说明.php

--- 说明.php
+++ 说明.php
@@ -7,14 +7,14 @@
 //查看消费记录和余额
 //会员注册时要能填生日信息。微信公众后台能设置自动给会员发生日祝福
 
-公众号帐号是:2066893454@qq.com 
-密码是:asdf123456789  
-appid:wx71ba0a52ebf23ab9
-appsecret:147614c1ef3ccd38c1319912543823ef
+公众号帐号是:
+密码是:  
+appid:
+appsecret:
 encodingaeskey:tVYBMvzeACHNOcKbcImtrFKTBmf4kpZix7B8qRbF1P0
 
-URL:http://linzi.yunw8.com/index.php/index/Index/yanzheng
-Token:linziyijia
+URL:
+Token:
 
 
 1、QTSaleOrder和QTAddCardAmount增加字段is_send,来检测发送模板消息；
